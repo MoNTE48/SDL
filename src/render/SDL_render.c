c88f48f2beaaa7c864edc71708ac475f31a32a8a
Sylvain <sylvain.becker@gmail.com>
2026-02-03 10:40:30 +0100

    Android: add RPC commands from SDLActivity thread to SDL Main C thread.
    - now SDLActivity defers most of SDL code internal execution to the Main thread.
      Since all code is now in Main thread, this prevents any race conditions within SDL code.
    
    - WatchEvents, including user ones, are now exected in SDL Main C thread
      This prevents race conditions that a user could add.
    
    - Simplify the locking/mutex internals between SDLActivity and Main.
      SDLActivity native code / synchronization is reduced to the minimum
      and fastest.
    
    - ordering between pause/resume and java surface create/changed/destroyed is kept.
      (prevent to do a "resume" before java surface isn't set up)
    
    - Fixed bug #11324 - Android opengles2: First SDL_RenderPresent() broken after resize
      Call ANativeWindow_setBuffersGeometry when size changes ( Android_NativeSurfaceResized )
    
    NB: you can define SDL_ANDROID_GAMEPAD_AS_RPC in src/core/android/SDL_android.c
    so that the code that sends gamepad events is moved to main SDLThread
    
    (see #14925, #14962)

--- src/render/SDL_render.c
+++ src/render/SDL_render.c
@@ -1075,7 +1075,7 @@ SDL_Renderer *SDL_CreateRendererWithProperties(SDL_PropertiesID props)
     }
 
 #ifdef SDL_PLATFORM_ANDROID
-    if (!Android_WaitActiveAndLockActivity()) {
+    if (!Android_WaitActiveAndLockActivity(window)) {
         return NULL;
     }
 #endif
@@ -1255,7 +1255,7 @@ SDL_Renderer *SDL_CreateRendererWithProperties(SDL_PropertiesID props)
     SDL_renderers = renderer;
 
 #ifdef SDL_PLATFORM_ANDROID
-    Android_UnlockActivityMutex();
+    Android_UnlockActivityState();
 #endif
 
     SDL_ClearError();
@@ -1264,7 +1264,7 @@ SDL_Renderer *SDL_CreateRendererWithProperties(SDL_PropertiesID props)
 
 error:
 #ifdef SDL_PLATFORM_ANDROID
-    Android_UnlockActivityMutex();
+    Android_UnlockActivityState();
 #endif
 
     if (renderer) {
